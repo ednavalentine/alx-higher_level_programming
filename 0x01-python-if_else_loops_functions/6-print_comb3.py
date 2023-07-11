b62c61d596d4a85ba6534b46532f162a2e9769aa
ednavalentine <valentineedna@gmail.com>
2023-07-12 01:50:55 +0300

    made changes to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for dif in range(10):
     for diff in range(dif+1, 10):
-        if dif == 0:
-            print("{:02d}, ".format(diff), end="")
-        else:
-            print("{:02d}, ".format(dif * 10 + diff), end="")
+        if not (dif == 8 and diff == 9):
+            print("{:d}{:d}".format(dif, diff), end=", ")
+print("{:d}{:d}".format(8, 9))
