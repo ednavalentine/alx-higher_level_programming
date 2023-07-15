0c55e97e8ec2721041db51ad49e63b26093e0b05
ednavalentine <valentineedna@gmail.com>
2023-07-15 19:35:09 +0300

    added an s and . to ) arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def main():
     argv = sys.argv[1:]
     num_args = len(argv)
     if num_args == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif num_args == 1:
         print("1 argument:")
         print(f"1: {argv[0]}")
