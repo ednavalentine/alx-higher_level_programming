4741c27fb4eb57912787b8ca7ecdb15b057819bf
ednavalentine <valentineedna@gmail.com>
2023-07-15 20:13:21 +0300

    changed the magic_calculations.add line to from magic_calculatior_102...

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for ink in range(4, 7):
+        for ink in range(4, 6):
             c = add(c, ink)
             return c
     else:
