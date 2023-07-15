269919c43a9ce59baea1dab31d6d2642b9172b05
ednavalentine <valentineedna@gmail.com>
2023-07-15 20:17:59 +0300

    changed ink to i

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,8 +3,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for ink in range(4, 6):
-            c = add(c, ink)
+        for i in range(4, 6):
+            c = add(c, i)
             return c
     else:
         return sub(a, b)
