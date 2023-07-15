cc5ec28560b728c6675366f0927f2c4450142ea4
ednavalentine <valentineedna@gmail.com>
2023-07-15 20:03:10 +0300

    Changed the code structure

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-import importlib.util
-
-
-def main():
-    file_path = "variable_load_5.py"
-    spec = importlib.util.spec_from_file_location("module_name", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    print(module.a)
-
-
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+    print(f"{a:d}")
