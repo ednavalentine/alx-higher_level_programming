d34c2760221d14c3d4d618c8fc341c0fce708711
ednavalentine <valentineedna@gmail.com>
2023-07-15 19:55:33 +0300

    changed the task structure

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-import marshal
-
-
-def print_names_from_pyc(file_path):
-    with open(file_path, 'rb') as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code_object = marshal.load(file)
-    names = code_object.co_names
-    filter_name = sorted(name for name in names if not name.startswith('__'))
-    for name in filter_name:
-        print(name)
+import hidden_4
 
 
 if __name__ == "__main__":
-    print_names_from_pyc("hidden_4.pyc")
+    namez = dir(hidden_4)
+    sort_names = sorted(name for name in namez if not name.startswith("__"))
+    for name in sort_names:
+        print(name)
