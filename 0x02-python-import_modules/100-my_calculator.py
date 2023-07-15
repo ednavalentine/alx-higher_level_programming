bc5f2ad8db7ef6027985b91df5f1efdecf0ec726
ednavalentine <valentineedna@gmail.com>
2023-07-15 17:35:36 +0300

    Task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+def perform_operation(a, operator, b):
+    if operator == "+":
+        return add(a, b)
+    elif operator == "-":
+        return sub(a, b)
+    elif operator == "*":
+        return mul(a, b)
+    elif operator == "/":
+        return div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    result = perform_operation(a, operator, b)
+    print(f"{a} {operator} {b} = {result}")
