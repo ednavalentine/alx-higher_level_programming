87dacac6f1f10fb302aea95f41704fe4eeee7c58
ednavalentine <valentineedna@gmail.com>
2023-07-15 15:55:18 +0300

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 0:
+        print("0 argument.")
+    elif num_args == 1:
+        print("1 argument:")
+        print(f"1: {argv[0]}")
+    else:
+        print(f"{num_args} arguments:")
+        for ink, arg in enumerate(argv, 1):
+            print(f"{ink}: {arg}")
+
+
+if __name__ == "__main__":
+    main()
