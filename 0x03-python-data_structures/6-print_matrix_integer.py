7cdbca58fb883b028d80a31684db2c8dafb73436
ednavalentine <valentineedna@gmail.com>
2023-07-17 17:11:57 +0300

    pycodestyle error solving

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for ink in matrix:
-            print(" ".join("{:d}".format(number) for number in ink))
+        print(" ".join("{:d}".format(number) for number in ink))
