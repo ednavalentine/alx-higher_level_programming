ba13333afdd97706f8591bae7500d59c963582a1
ednavalentine <valentineedna@gmail.com>
2023-07-21 12:42:56 +0300

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    prevalue = 0
+    for ink in roman_string[::-1]:
+        value = roman_numbers[ink]
+        if value < prevalue:
+            res -= value
+        else:
+            res += value
+        prevalue = value
+    return res
